38eec356b1ab050ae834dec9f2501e96fb3785be
Gerolmed <werwolf420@yahoo.de>
2021-12-16 18:22:57 +0100

    Setup base project

---
+++ CrossPlatformDesktop/DesktopGame.cs
@@ -0,0 +1,9 @@
+﻿using Core;
+
+namespace CrossPlatformDesktop
+{
+    public class DesktopGame : BaseGame
+    {
+        
+    }
+}
---
+++ CrossPlatformDesktop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CrossPlatformDesktop
+{
+    public static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            using var game = new DesktopGame();
+            game.Run();
+        }
+    }
+}
